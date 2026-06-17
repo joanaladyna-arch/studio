486699266798db5138a47c69e4ccfc3ff0c6f7eb
Joana Ladyna <joanaladyna@gmail.com>
2026-06-17 23:44:03 +0000

    Corrige le cache des icones

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,7 +44,7 @@ export default function RootLayout({
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="theme-color" content="#fdf2f5" />
         
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon-v2.png" />
         
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,12 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#d68fa3',
     icons: [
       {
-        src: '/icon-192.png',
+        src: '/icon-192-v2.png',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/icon-512.png',
+        src: '/icon-512-v2.png',
         sizes: '512x512',
         type: 'image/png',
       },
